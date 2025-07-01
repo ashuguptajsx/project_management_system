72b7c7d324412b845cd03af85481a7f12181b8f6
ashuguptajsx <ashugupta1403@gmail.com>
2025-07-01 10:28:07 +0530

    commit

---
+++ client/src/app/(components)/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-center bg-white px-4 py-'>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ client/src/app/DashboardWrapper.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from "@/app/(components)/Navbar";
+
+
+
+const DashboardWrapper = ({children}:{children:React.ReactNode}) => {
+  return (
+    <div className='flex min-h-screen bg-gray-50 text-gray-900'>
+        sidebar
+        <main className="flex w-full flex-col  bg-gray-50 md:pl-64">
+            <Navbar />
+            {children}
+        </main>
+    </div>
+  )
+}
+
+export default DashboardWrapper
