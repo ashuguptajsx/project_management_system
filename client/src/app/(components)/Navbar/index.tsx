e7f92e45e0bb469d83c9e8e5a34aa54258c1eeb5
ashuguptajsx <ashugupta1403@gmail.com>
2025-07-01 10:28:17 +0530

    commit

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { SearchIcon } from 'lucide-react'
 
 
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-center bg-white px-4 py-'>Navbar</div>
+    <div className='flex items-center justify-between bg-white px-4 py-3'>
+     <div className='flex items-center gap-8'>
+      <div className ="relative flex h-min w-[200px]">
+        <SearchIcon className='absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer'/>
+      </div>
+     </div>
+    </div>
   )
 }
 
