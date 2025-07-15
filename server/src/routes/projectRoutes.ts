7af2226e89ec903a089af338624db2979ae2c35a
ashuguptajsx <ashugupta1403@gmail.com>
2025-07-15 15:16:37 +0530

    commit

---
+++ server/src/routes/projectRoutes.ts
@@ -0,0 +1,11 @@
+import {Router} from 'express';
+import { getProjects } from '../controllers/projectController';
+import { createProject } from '../controllers/projectController';
+
+const router = Router();
+
+router.get('/', getProjects);
+router.post("/", createProject);
+
+
+export default router;
