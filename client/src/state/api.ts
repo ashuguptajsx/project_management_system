96c856e03008142357b42b1dfea9d982e6dde34f
ashuguptajsx <ashugupta1403@gmail.com>
2025-07-18 23:06:36 +0530

    commit

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { get } from "http";
 
 export interface Project{
      id: number;
@@ -28,6 +29,26 @@ export enum Status{
 
 
 
+
+export interface User{
+        UserId? :  number;
+        username : string,
+        email : string,
+       
+        profilePictureUrl? : string,
+        cognitoId? : string,
+        teamId? : number,
+}
+
+
+export interface Attachment{
+    id: number,
+    fireUrl : string,
+    fileName : string,
+    taskId : number,
+    uploadedById : number
+}
+
 export interface Task{
      id: number;
      title : string,
@@ -41,17 +62,44 @@ export interface Task{
      projectId? : number,
      authorUserId? : number,
      assignedUserId? : number,
+
+
+     author? : User;
+     assignee? : User;
+     comments? : Comment[];
+     attachments? : Attachment[];
 }
 
 
-
+ 
 export const api = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL
     }),
     reducerPath: "api",
-    tagTypes : [],
-    endpoints: (build) =>({})
+    tagTypes : ["Projects", "Tasks"],
+    endpoints: (build) =>({ 
+        getProjects: build.query<Project[], void>({
+            query: () => "projects",
+            providesTags: ["Projects"] 
+        }),
+        createProject : build.mutation<Project, Partial<Project>>({
+            query: (project) =>({
+                url : "projects",
+                method: "POST",
+                body: project
+            }),
+            invalidatesTags: ["Projects"]
+        }),
+        getTasks : build.query<Task[], {projectId : number}>({
+            query : ({projectId}) => `tasks?projectId=${projectId}`,
+            providesTags: (result) => result ? result.map(({id}) =>({type : "Tasks" as const, id})) : [{type : "Tasks" as const}]
+
+        })
+    })
 }) 
 
-export const {} = api;
+export const {
+    useGetProjectsQuery,
+    useCreateProjectMutation
+} = api;
